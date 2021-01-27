d07a078dfaa5a01265e49d51553321bf7adc0b48
visiont3lab <55084651+visiont3lab@users.noreply.github.com>
2021-01-27 21:16:24 +0100

    Update covid.py

--- covid.py
+++ covid.py
@@ -1,8 +1,19 @@
 from plotly import graph_objects as go
 import pandas as pd
 import numpy as np 
+from datetime import datetime
+import os
 
+# Configurazione ambiente virtuale
 # !pip install plotly==4.12.0 kaleido
+# virtualenv env
+# virtualenv --python=python3 env
+# WINDOWS: source env/Scripts/activate
+# MACOS/LINUX source env/bin/activate
+
+# Installazione librerie
+# pip install plotly==4.12.0 kaleido pandas
+# pip list
 
 def customPlot(df,toplot,diz,nameSave="Ciao"):
     #print(diz)
@@ -35,28 +46,37 @@ def customPlot(df,toplot,diz,nameSave="Ciao"):
         yaxis_showgrid=False,
         hovermode="x",  
     )
-    fig.write_html(nameSave + ".html")
-    fig.write_image(nameSave + ".png")
+
+    d = datetime.now()
+    dstring = d.strftime("%Y-%m-d_%H_%M_SS")
+    fig.write_html("results/" + nameSave +dstring + ".html")
+    fig.write_image("results/" + nameSave + dstring+ ".png")
 
     return fig
 
-# Read data
-url ="https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
-df = pd.read_csv(url) # lettura
+if __name__ == '__main__':
+    try:
+        os.makedirs("results")
+    except OSError as e:
+        print("directory already  exist", e)
 
-# Creazione dizionario
-l1 = ["deceduti", "totale_positivi", "totale_casi","variazione_totale_positivi","nuovi_positivi"]
-l2 = ["rgb(255,0,0)","rgb(0,255,221)","rgb(0,255,15)","rgb(0,255,155)","rgb(0,255,0)"]
-diz = {  k: v for k,v in zip(l1,l2) }
+    # Read data
+    url ="https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
+    df = pd.read_csv(url) # lettura
 
-# Figura 1 
-toplot1 = ["deceduti", "totale_positivi", "totale_casi"]
-fig1 = customPlot(df,toplot1,diz, nameSave="Andamento-Generale")
+    # Creazione dizionario
+    l1 = ["deceduti", "totale_positivi", "totale_casi","variazione_totale_positivi","nuovi_positivi"]
+    l2 = ["rgb(255,0,0)","rgb(0,255,221)","rgb(0,255,15)","rgb(0,255,155)","rgb(0,255,0)"]
+    diz = {  k: v for k,v in zip(l1,l2) }
 
-# Figura 2
-toplot2 = ["variazione_totale_positivi"]
-fig2 = customPlot(df,toplot2,diz,nameSave="Variazione-Totale-Positivi")
+    # Figura 1 
+    toplot1 = ["deceduti", "totale_positivi", "totale_casi"]
+    fig1 = customPlot(df,toplot1,diz, nameSave="Andamento-Generale")
 
-# Figura 3
-toplot3 = ["nuovi_positivi"]
-fig3 = customPlot(df,toplot3,diz,nameSave="Nuovi-Positivi")
+    # Figura 2
+    toplot2 = ["variazione_totale_positivi"]
+    fig2 = customPlot(df,toplot2,diz,nameSave="Variazione-Totale-Positivi")
+
+    # Figura 3
+    toplot3 = ["nuovi_positivi"]
+    fig3 = customPlot(df,toplot3,diz,nameSave="Nuovi-Positivi")
