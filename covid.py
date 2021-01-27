1c73d967a9e9449c594dab5f4ef21f724d1c5b24
visiont3lab <55084651+visiont3lab@users.noreply.github.com>
2021-01-27 20:46:19 +0100

    Create covid.py

---
+++ covid.py
@@ -0,0 +1,62 @@
+from plotly import graph_objects as go
+import pandas as pd
+import numpy as np 
+
+# !pip install plotly==4.12.0 kaleido
+
+def customPlot(df,toplot,diz,nameSave="Ciao"):
+    #print(diz)
+    fig = go.Figure()
+    # Mettiamo le tracce
+    for v in toplot:
+        fig.add_traces(
+            go.Scatter(
+                x=df["data"].values,
+                y=df[v].values,
+                name=v,
+                mode="lines",
+                line_color=diz[v]
+            )
+        )
+    # Aggiorniamo il layout
+    fig.update_layout(
+        title_text=nameSave,
+        title_font_size=50,
+        font_color="white",
+        paper_bgcolor="black",
+        plot_bgcolor="white",
+        xaxis_showgrid=False,
+        #xaxis_range=['2021-01-01','2021-01-31'],
+        xaxis= dict(
+                dtick="M1",
+                tickformat="%d-%m-%Y",
+                ticklabelmode="period",
+            ),
+        yaxis_showgrid=False,
+        hovermode="x",  
+    )
+    fig.write_html(nameSave + ".html")
+    fig.write_image(nameSave + ".png")
+
+    return fig
+
+# Read data
+url ="https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv"
+df = pd.read_csv(url) # lettura
+
+# Creazione dizionario
+l1 = ["deceduti", "totale_positivi", "totale_casi","variazione_totale_positivi","nuovi_positivi"]
+l2 = ["rgb(255,0,0)","rgb(0,255,221)","rgb(0,255,15)","rgb(0,255,155)","rgb(0,255,0)"]
+diz = {  k: v for k,v in zip(l1,l2) }
+
+# Figura 1 
+toplot1 = ["deceduti", "totale_positivi", "totale_casi"]
+fig1 = customPlot(df,toplot1,diz, nameSave="Andamento-Generale")
+
+# Figura 2
+toplot2 = ["variazione_totale_positivi"]
+fig2 = customPlot(df,toplot2,diz,nameSave="Variazione-Totale-Positivi")
+
+# Figura 3
+toplot3 = ["nuovi_positivi"]
+fig3 = customPlot(df,toplot3,diz,nameSave="Nuovi-Positivi")
