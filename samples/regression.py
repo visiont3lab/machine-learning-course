6d8053e720e020940305716084406189d7bb3c37
visiont3lab <55084651+visiont3lab@users.noreply.github.com>
2021-02-01 21:09:29 +0100

    Update regression.py

--- samples/regression.py
+++ samples/regression.py
@@ -1,4 +1,5 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression,Lasso
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from plotly import graph_objects as go
 import pandas as pd
@@ -21,20 +22,28 @@ fig.add_traces(go.Scatter(
 # ---- Prepara i dati
 X = np.linspace(-5,5, num=len(df["variazione_totale_positivi"].values)).reshape(-1,1) # 2D matrice (344,1) row (dati raccolti),col (gli input)
 y = df["variazione_totale_positivi"].values # --(344,)
+# Creazioen train test set
 # ---------
 
 # --- Preprocessing
 # Polynomial Feature --> Crea nuovi input partendo da quelli che gia abbiamo
-poly = PolynomialFeatures(degree=25) # 2,3,4 -->5 --700
+poly = PolynomialFeatures(degree=2) # 2,3,4 -->5 --700
 X_new = poly.fit_transform(X)
+print(X_new.shape)
 #Xnew = np.array([X,np.power(X,2),np.power(X,3),np.power(X,4),np.power(X,5),np.power(X,6),np.power(X,7),np.power(X,8)]).T  # 4,344 --> 344,4
 # PCA riduce il numero di input
 # ------------------
 
 # Regression 
+#v = RandomForestRegressor()
 v = LinearRegression()
+#v = ExtraTreesRegressor()
 v.fit(X_new,y)  # mettere dei valori allinterno delle variabli coef_,  intercept  -> w0,w1 ---- wn
-Y_pred = v.predict(X_new) # coef_, intercept_
+Y_pred = v.predict(X_new) #np.array([8,8*8,8*8*8]).reshape(-1,3)) # coef_, intercept_
+#print(Y_pred)
+# Y_pred = w0 + w1*X  ---_>
+#Y_n = v.intercept_ + v.coef_[0]*8 + v.coef_[1]*8*8 + v.coef_[2]*8*8*8
+#print(Y_n)
 # Y_pred = w0 + w1*X + w2*np.power(X,2) + w3*np.power(X,3) + w4*np.power(X,4) + w5*np.power(X,5) + w6*np.power(X,6) + w7*np.power(X,7) + w8*np.power(X,8)
 # --------
 
